fe4128f01ab4b0cd8156341de68dc164a44c87e8
ryanlee2014 <gxlhybh@gmail.com>
2017-08-14 16:59:14 +0800

    no message

---
+++ HDU/HDU6115.cpp
@@ -0,0 +1,552 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <list>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <complex>
+#define in(x) scanf("%d",&x)
+#define ein(x) ~scanf("%d",&x)
+#define inl(x) scanf("%lld",&x)
+#define ins(x) scanf("%s",x)
+#define inf(x) scanf("%lf",&x)
+#define inc(x) scanf("%c",&x)
+#define gc() getchar()
+#define out(x) printf("%d\n",x)
+#define outl(x) printf("%lld\n",x)
+#define od(x, y) printf("%d %d\n",x,y)
+#define ms(x) memset(x,0,sizeof(x))
+#define msc(x, n) memset(x,n,sizeof(x))
+#define cp(x) while(!x.empty())x.pop()
+#define rep(i, a, b) for(int i=a;i<b;++i)
+#define rvep(i, a, b) for(int i=a;i>=b;--i)
+#define elif else if
+#define el else
+#define wl(x) while(x)
+#define sn scanf
+#define bl bool
+#define mp make_pair
+#define pii pair<int,int>
+#define rtn return
+#define ope operator
+#define cst const
+#define it int
+#define cr char
+#define ctn continue
+#define stt struct
+#define il inline
+#define tpl template
+#define cl class
+#define db double
+#define sf(x) sizeof(x)
+#define vt void
+#define pf printf
+typedef long long ll;
+using namespace std;
+cst it INF = 0x3f3f3f3f;
+/*
+ * 输入挂
+ * 场场AK buff
+ */
+
+class endln
+{
+};
+cst it MAXBUF = 100000;
+cst it MAXOUT = 100000;
+class iofstream
+{
+private:
+	it idx;
+	bl eof;
+	cr buf[MAXBUF], *ps = buf, *pe = buf + 1;
+	cr bufout[MAXOUT], outtmp[50], *pout = bufout, *pend = bufout + MAXOUT;
+	il vt rnext()
+	{
+		if (++ps == pe)
+			pe = (ps = buf) + fread(buf, sf(cr), sf(buf) / sf(cr), stdin),eof=true;
+		if(ps==pe)
+			eof=false;
+	}
+	il vt write()
+	{
+		fwrite(bufout, sf(cr), pout - bufout, stdout);
+		pout = bufout;
+	}
+public:
+	iofstream(char* in=NULL,char* out=NULL) : idx(-1),eof(true)
+	{
+		if(in)
+			freopen(in,"r",stdin);
+		if(out)
+			freopen(out,"w",stdout);
+	}
+	tpl<cl T>
+	il bl fin(T &ans)
+	{
+#ifdef ONLINE_JUDGE
+		ans = 0;
+    T f = 1;
+    if (ps == pe)
+    {
+        eof=false;
+        rtn false;//EOF
+	}
+    do
+    {
+        rnext();
+        if ('-' == *ps) f = -1;
+    } wl(!isdigit(*ps) && ps != pe);
+    if (ps == pe)
+     {
+        eof=false;
+         rtn false;//EOF
+	}
+    do
+    {
+        ans = (ans << 1) + (ans << 3) + *ps - 48;
+        rnext();
+    } wl(isdigit(*ps) && ps != pe);
+    ans *= f;
+#else
+		cin >> ans;
+#endif
+		rtn true;
+	}
+
+	tpl<cl T>
+	il bl fdb(T &ans)
+	{
+#ifdef ONLINE_JUDGE
+		ans = 0;
+    T f = 1;
+    if (ps == pe) rtn false;//EOF
+    do
+    {
+        rnext();
+        if ('-' == *ps) f = -1;
+    } wl(!isdigit(*ps) && ps != pe);
+    if (ps == pe) rtn false;//EOF
+    do
+    {
+        ans = ans*10 + *ps - 48;
+        rnext();
+    } wl(isdigit(*ps) && ps != pe);
+    ans *= f;
+        if(*ps=='.')
+        {
+            rnext();
+            T small=0;
+            do
+            {
+                small=small*10+*ps-48;
+                rnext();
+            }wl(isdigit(*ps)&&ps!=pe);
+            wl(small>=1)
+            {
+                small/=10;
+            }
+            ans+=small;
+        }
+#else
+		cin >> ans;
+#endif
+		rtn true;
+	}
+
+/*
+ * 输出挂
+ * 超强 超快
+ */
+
+	il bl out_char(cst cr c)
+	{
+#ifdef ONLINE_JUDGE
+		*(pout++) = c;
+    if (pout == pend) write();
+    write();
+#else
+		cout << c;
+#endif
+		rtn true;
+	}
+	il bl out_str(cst cr *s)
+	{
+#ifdef ONLINE_JUDGE
+		wl(*s)
+    {
+        *(pout++) = *(s++);
+        if (pout == pend) write();
+    }
+    write();
+#else
+		cout << s;
+#endif
+		rtn true;
+	}
+
+	tpl<cl T>
+	il bl out_double(T x, it idx)
+	{
+		char str[50];
+		string format = "%";
+		if(~idx)
+		{
+			format+='.';
+			format += (char) (idx + '0');
+		}
+		format += "f";
+		sprintf(str, format.c_str(), x);
+		out_str(str);
+	}
+
+	tpl<cl T>
+	il bl out_int(T x)
+	{
+#ifdef ONLINE_JUDGE
+		if (!x)
+        {
+            out_char('0');
+            rtn true;
+        }
+        if (x < 0) x = -x, out_char('-');
+        it len = 0;
+        wl(x)
+        {
+            outtmp[len++] = x % 10 + 48;
+            x /= 10;
+        }
+        outtmp[len] = 0;
+        for (it i = 0, j = len - 1; i < j; i++, j--) swap(outtmp[i], outtmp[j]);
+        out_str(outtmp);
+#else
+		cout << x;
+#endif
+		rtn true;
+	}
+
+	tpl<cl T>
+	il bl out_intln(T x)
+	{
+#ifdef ONLINE_JUDGE
+		out_int(x),out_char('\n');
+        write();
+#else
+		cout << x << endl;
+#endif
+		rtn true;
+	}
+
+	tpl<cl T>
+	il bl out_doubleln(T x, it idx)
+	{
+		out_double(x, idx), out_char('\n');
+	}
+	il iofstream &ope<<(cst db &x)
+	{
+		out_double(x, idx);
+		rtn *this;
+	}
+
+	il iofstream &ope<<(cst it &x)
+	{
+		out_int(x);
+		rtn *this;
+	}
+
+	il iofstream& ope<<(cst unsigned long long& x)
+	{
+		out_int(x);
+		rtn *this;
+	}
+
+	il iofstream& ope<<(cst unsigned& x)
+	{
+		out_int(x);
+		rtn *this;
+	}
+
+	il iofstream& ope<<(cst long& x)
+	{
+		out_int(x);
+		rtn *this;
+	}
+
+	il iofstream &ope<<(cst ll &x)
+	{
+		out_int(x);
+		rtn *this;
+	}
+
+	il iofstream &ope<<(cst endln &x)
+	{
+		out_char('\n');
+		rtn *this;
+	}
+
+
+	il iofstream &ope<<(cst cr *x)
+	{
+		out_str(x);
+		rtn *this;
+	}
+
+	il iofstream& ope<<(cst string& x)
+	{
+		out_str(x.c_str());
+		rtn *this;
+	}
+
+	il iofstream &ope<<(cst char &x)
+	{
+		out_char(x);
+		rtn *this;
+	}
+
+	il bl setw(it x)
+	{
+		if (x >= 0)
+		{
+			idx = x;
+			rtn true;
+		}
+		rtn false;
+	}
+
+	il iofstream &ope>>(it &x)
+	{
+		if(!fin(x))eof=false;
+		rtn *this;
+	}
+
+	il iofstream &ope>>(ll &x)
+	{
+		if(!fin(x))eof=false;
+		rtn *this;
+	}
+
+	il iofstream& ope>>(db& x)
+	{
+		if(!fdb(x))eof=false;
+		rtn *this;
+	}
+
+	il iofstream& ope>>(float& x)
+	{
+		if(!fdb(x))eof=false;
+		rtn *this;
+	}
+
+	il iofstream &ope>>(unsigned &x)
+	{
+		if(!fin(x))eof=false;
+		rtn *this;
+	}
+
+	il iofstream &ope>>(unsigned long long &x)
+	{
+		if(!fin(x))eof=false;
+		rtn *this;
+	}
+	il ope bl()
+	{
+		rtn eof;
+	}
+
+	il cr getchar()
+	{
+#ifdef ONLINE_JUDGE
+		if (ps == pe){
+			eof=false;//EOF
+			rtn 0;
+		}
+		rnext();
+		if(ps+1==pe)
+			eof=false;
+		rtn *ps;
+#else
+		rtn std::getchar();
+#endif
+	}
+
+	il iofstream& ope>>(char* str)
+	{
+#ifdef ONLINE_JUDGE
+		if (ps == pe){
+			eof=false;//EOF
+			rtn *this;
+		}
+		do
+		{
+			rnext();
+		} wl(isspace(*ps)&&iscntrl(*ps) && ps != pe);
+		if (ps == pe){
+			eof=false;//EOF
+			rtn *this;
+		}
+		do
+		{
+			*str=*ps;
+			++str;
+			rnext();
+		} wl(!(isspace(*ps)||iscntrl(*ps)) && ps != pe);
+		*str='\0';
+		rtn *this;
+#else
+		cin>>str;
+		rtn *this;
+#endif
+	}
+
+	il iofstream& ope>>(string& str)
+	{
+#ifdef ONLINE_JUDGE
+		str.clear();
+		if (ps == pe){
+			eof=false;//EOF
+			rtn *this;
+		}
+		do
+		{
+			rnext();
+		} wl(isspace(*ps)&&iscntrl(*ps) && ps != pe);
+		if (ps == pe){
+			eof=false;//EOF
+			rtn *this;
+		}
+		do
+		{
+			str+=*ps;
+			rnext();
+		} wl(!(isspace(*ps)||iscntrl(*ps)) && ps != pe);
+		rtn *this;
+#else
+		cin>>str;
+		rtn *this;
+#endif
+	}
+
+	il iofstream& getline(char* str)
+	{
+#ifdef ONLINE_JUDGE
+		if (ps == pe){
+			eof=false;//EOF
+			rtn *this;
+		}
+		do
+		{
+			rnext();
+			*str=*ps;
+		}while(*ps!='\n'&&ps!=pe&&str++);
+		*str='\0';
+		rtn *this;
+#else
+		gets(str);
+		rtn *this;
+#endif
+	}
+	il bl endfile()
+	{
+		rtn eof;
+	}
+};
+static iofstream fin;
+static endln ln;
+
+int fa[100010],anc[100010][32],deep[100010],dis[100010];
+struct Node
+{
+	Node(){}
+	Node(it to,it val):to(to),val(val){}
+	int to,val;
+};
+vector<Node>edge[100010];
+vector<it>vec[100010];
+int T,n,m;
+il vt dfs(it x)
+{
+	anc[x][0]=fa[x];
+	rep(i,1,32)
+		anc[x][i]=anc[anc[x][i-1]][i-1];
+	it len=edge[x].size();
+	rep(i,0,len)
+	{
+		Node& next=edge[x][i];
+		if(next.to!=fa[x]){
+			fa[next.to]=x;
+			deep[next.to]=deep[x]+1;
+			dis[next.to]=dis[x]+next.val;
+			dfs(next.to);
+		}
+	}
+}
+it LCA(it x,it y){
+	if(deep[x]<deep[y])
+		swap(x,y);
+	rvep(i,31,0)
+	{
+		if(deep[y]<=deep[anc[x][i]]){
+			x=anc[x][i];
+		}
+	}
+	if(x==y)
+		return x;
+	rvep(i,31,0)
+	{
+		if(anc[x][i]!=anc[y][i]){
+			x=anc[x][i];
+			y=anc[y][i];
+		}
+	}
+	rtn anc[x][0];
+}
+il vt init(){
+	rep(i,0,n+1)
+	{
+		edge[i].clear();
+		vec[i].clear();
+	}
+	ms(fa),ms(anc);
+}
+it main(vt)
+{
+	fin>>T;
+	it u,v,w,x,y;
+	wl(T--)
+	{
+		fin>>n>>m;
+		init();
+		rep(i,1,n)
+		{
+			fin>>u>>v>>w;
+			edge[u].push_back((Node){v,w});
+			edge[v].push_back((Node){u,w});
+		}
+		rep(i,1,m+1)
+		{
+			fin>>x;
+			rep(j,1,x+1)
+			{
+				fin>>y;
+				vec[i].push_back(y);
+			}
+		}
+		fa[1]=1;
+		dfs(1);
+		it q;
+		fin>>q;
+		wl(q--)
+		{
+			fin>>u>>v;
+			it l1=vec[u].size(),l2=vec[v].size(),ans=INF;
+			rep(i,0,l1)rep(j,0,l2)ans=min(ans,dis[vec[u][i]]+dis[vec[v][j]]-(dis[LCA(vec[u][i],vec[v][j])]<<1));
+			fin<<ans<<ln;
+		}
+	}
+	rtn 0;
+}
